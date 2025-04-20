13ced6f8f854971120739170e46037b19f32de4f
TY <185649066@qq.com>
2025-04-20 23:33:10 +0800

    StatisticRecommender: modify spark session name

--- recommender/StatisticRecommender/src/main/scala/com/ty/statistics/StatisticRecommender.scala
+++ recommender/StatisticRecommender/src/main/scala/com/ty/statistics/StatisticRecommender.scala
@@ -39,7 +39,7 @@ object StatisticRecommender {
     implicit val mongoConfig: MongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
     val sparkConf = new SparkConf()
       .setMaster(config("spark.cores"))
-      .setAppName("DataLoader")
+      .setAppName("StatisticRecommender")
     val sparkSession = new SparkSession.Builder().config(sparkConf).getOrCreate()
     import sparkSession.implicits._
     // 从 mongo 中加载数据
@@ -89,7 +89,7 @@ object StatisticRecommender {
       .mapGroups { (genres, iter) =>
         val topMovies = iter
           .map(_._2)
-          .toArray
+          .toList
           .sortBy(-_._2)
           .take(10)
           .map { case (mId, score) => Recommendation(mId, score) }
